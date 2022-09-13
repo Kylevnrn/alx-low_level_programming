3263ae745d2e109f7501bfc768edf43b15c752a2
Kylevnrn <kylevnrn@gmail.com>
2022-09-13 13:03:15 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,12 @@
-#include "holberton.h"
+#include<stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
-	char *sh = "Holberton";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
+	printf("_putchar\n");
 	return (0);
 }
